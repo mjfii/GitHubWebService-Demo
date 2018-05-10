8fe11ef0700d01a9556f4c6f3571af4dcb9a08b2
mjfii <mick.flanigan@gmail.com>
2018-05-10 11:46:06 -0400

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -8,6 +8,7 @@ from gidgethub import aiohttp as gh_aiohttp
 
 router = routing.Router()
 
+
 @router.register("issues", action="opened")
 async def issue_opened_event(event, gh, *args, **kwargs):
     """
@@ -18,6 +19,18 @@ async def issue_opened_event(event, gh, *args, **kwargs):
 
     message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
     await gh.post(url, data={"body": message})
+
+
+@router.register("pull_request", action="closed")
+async def pr_closed_event(event, gh, *args, **kwargs):
+    user = event.data["pull_request"]["user"]["login"]
+    is_merged = event.data["pull_request"]["merged"]
+    url = event.data["pull_request"]["comments_url"]
+
+    if is_merged:
+        message = f"Thanks for the PR @{user}! (I'm a bot)."
+        await gh.post(url, data={"body": message})
+
 
 async def main(request):
     body = await request.read()
